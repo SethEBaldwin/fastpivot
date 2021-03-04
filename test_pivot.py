cf047c0222bfc24625d58812c2b186083d5360d9
SethEBaldwin <seth.e.baldwin@gmail.com>
2021-03-04 16:06:40 -0500

    convert to pytest format

--- test_pivot.py
+++ test_pivot.py
@@ -3,40 +3,47 @@ import pandas as pd
 import numpy as np
 import time
 
-# create data
-    
-column1 = 'to_be_idx'
-column2 = 'to_be_col'
-column3 = 'value'
+def test_pivot():
 
-n_rows = 100
-n_cols = 10
-n_idx = 10
-col1 = ['idx{}'.format(x) for x in np.random.randint(0, n_idx, size=n_rows)]
-col2 = ['col{}'.format(x) for x in np.random.randint(0, n_cols, size=n_rows)]
-col3 = [x for x in np.random.normal(size=n_rows)]
+    # create data
 
-data = np.transpose([col1, col2, col3])
-df = pd.DataFrame(data, columns=[column1, column2, column3], index=range(len(data)))
-df[column3] = df[column3].astype(np.float64)
-#print(df)
+    column1 = 'to_be_idx'
+    column2 = 'to_be_col'
+    column3 = 'value'
 
-# time
+    n_rows = 100
+    n_cols = 10
+    n_idx = 10
+    col1 = ['idx{}'.format(x) for x in np.random.randint(0, n_idx, size=n_rows)]
+    col2 = ['col{}'.format(x) for x in np.random.randint(0, n_cols, size=n_rows)]
+    col3 = [x for x in np.random.normal(size=n_rows)]
 
-msg = 'cython'
-tick = time.perf_counter()
-pivot_cython = pivot.pivot(df, index=column1, column=column2, value=column3)
-print(msg, time.perf_counter() - tick)
-#print(pivot_cython)
+    data = np.transpose([col1, col2, col3])
+    df = pd.DataFrame(data, columns=[column1, column2, column3], index=range(len(data)))
+    df[column3] = df[column3].astype(np.float64)
+    #print(df)
 
-msg = 'pandas'
-tick = time.perf_counter()
-pivot_pandas = df.pivot_table(index=column1, columns=[column2], values=column3, fill_value=0.0, aggfunc='sum')
-print(msg, time.perf_counter() - tick)
-#print(pivot_pandas)
+    # time
 
-# check results are equal
+    msg = 'cython'
+    tick = time.perf_counter()
+    pivot_cython = pivot.pivot(df, index=column1, column=column2, value=column3)
+    print(msg, time.perf_counter() - tick)
+    #print(pivot_cython)
 
-print('componentwise equal: ', (pivot_cython.to_numpy() == pivot_pandas.to_numpy()).all())
-epsilon = 1e-8
-print('componentwise within {} :'.format(epsilon), (np.absolute(pivot_cython.to_numpy() - pivot_pandas.to_numpy()) < epsilon).all())
+    msg = 'pandas'
+    tick = time.perf_counter()
+    pivot_pandas = df.pivot_table(index=column1, columns=[column2], values=column3, fill_value=0.0, aggfunc='sum')
+    print(msg, time.perf_counter() - tick)
+    #print(pivot_pandas)
+
+    # check results are equal
+
+    is_equal = (pivot_cython.to_numpy() == pivot_pandas.to_numpy()).all()
+    print('componentwise equal: ', is_equal)
+    epsilon = 1e-8
+    within_epsilon = (np.absolute(pivot_cython.to_numpy() - pivot_pandas.to_numpy()) < epsilon).all()
+    print('componentwise within {} :'.format(epsilon), within_epsilon)
+
+    assert is_equal
+    assert within_epsilon
