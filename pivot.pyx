9962e173435309e000fc7956d6876eb73ab55313
SethEBaldwin <seth.e.baldwin@gmail.com>
2021-03-04 13:05:10 -0500

    better testing for pivot

--- pivot.pyx
+++ pivot.pyx
@@ -9,69 +9,69 @@ import pandas as pd
 import numpy as np
 import time
 
-cdef cmap[string, int] dict_to_cmap(dict the_dict):
-    # the_dict is a dictionary mapping strings to ints
-    cdef string map_key
-    cdef int map_val
-    cdef cpair[string, int] map_element
-    cdef cmap[string, int] my_map
-    for key,val in the_dict.items():
-        map_key = key
-        map_val = val
-        map_element = (map_key, map_val)
-        my_map.insert(map_element)
-    return my_map
+# cdef cmap[string, int] dict_to_cmap(dict the_dict):
+#     # the_dict is a dictionary mapping strings to ints
+#     cdef string map_key
+#     cdef int map_val
+#     cdef cpair[string, int] map_element
+#     cdef cmap[string, int] my_map
+#     for key,val in the_dict.items():
+#         map_key = key
+#         map_val = val
+#         map_element = (map_key, map_val)
+#         my_map.insert(map_element)
+#     return my_map
 
-def array_to_vector(arr):
-    # arr is a 1d numpy array of strings
-    cdef string element
-    cdef int i
-    cdef vector[string] my_vector
-    for i in range(arr.shape[0]):
-        element = arr[i]
-        my_vector.push_back(element)
-    return my_vector
+# def array_to_vector(arr):
+#     # arr is a 1d numpy array of strings
+#     cdef string element
+#     cdef int i
+#     cdef vector[string] my_vector
+#     for i in range(arr.shape[0]):
+#         element = arr[i]
+#         my_vector.push_back(element)
+#     return my_vector
 
-def enumerate(arr):
-    # takes 1d np array of strings and returns an array of ints, where 0 corresponds to first unique element of arr, 1 the second, etc.
-    cdef cmap[string, int] enumeration_map
-    cdef cpair[string, int] map_pair
-    cdef string value
-    cdef int is_old
-    cdef long count = 0
-    cdef int n = arr.shape[0]
-    cdef long[:] return_arr = np.empty(n, dtype=np.int)
-    cdef int i
-    for i in range(n):
-        value = arr[i]
-        is_old = enumeration_map.count(value)
-        if is_old:
-            return_arr[i] = enumeration_map[value]
-        else:
-            return_arr[i] = count
-            map_pair = (value, count)
-            enumeration_map.insert(map_pair)
-            count += 1            
-    return return_arr
+# def enumerate(arr):
+#     # takes 1d np array of strings and returns an array of ints, where 0 corresponds to first unique element of arr, 1 the second, etc.
+#     cdef cmap[string, int] enumeration_map
+#     cdef cpair[string, int] map_pair
+#     cdef string value
+#     cdef int is_old
+#     cdef long count = 0
+#     cdef int n = arr.shape[0]
+#     cdef long[:] return_arr = np.empty(n, dtype=np.int)
+#     cdef int i
+#     for i in range(n):
+#         value = arr[i]
+#         is_old = enumeration_map.count(value)
+#         if is_old:
+#             return_arr[i] = enumeration_map[value]
+#         else:
+#             return_arr[i] = count
+#             map_pair = (value, count)
+#             enumeration_map.insert(map_pair)
+#             count += 1            
+#     return return_arr
 
-def enumerate2(arr, unique_arr):
-    # takes 1d np array of strings 1d array of unique strings from arr.
-    # returns an array of ints, where 0 corresponds to first unique element of arr, 1 the second, etc.
-    cdef cmap[string, int] enumeration_map
-    cdef cpair[string, int] map_pair
-    cdef string value
-    cdef int n = arr.shape[0]
-    cdef int m = unique_arr.shape[0]
-    cdef long[:] return_arr = np.empty(n, dtype=np.int)
-    cdef int i, j
-    for j in range(m):
-        value = unique_arr[j]
-        map_pair = (value, j)
-        enumeration_map.insert(map_pair)
-    for i in range(n):
-        value = arr[i]
-        return_arr[i] = enumeration_map[value]
-    return return_arr
+# def enumerate2(arr, unique_arr):
+#     # takes 1d np array of strings 1d array of unique strings from arr.
+#     # returns an array of ints, where 0 corresponds to first unique element of arr, 1 the second, etc.
+#     cdef cmap[string, int] enumeration_map
+#     cdef cpair[string, int] map_pair
+#     cdef string value
+#     cdef int n = arr.shape[0]
+#     cdef int m = unique_arr.shape[0]
+#     cdef long[:] return_arr = np.empty(n, dtype=np.int)
+#     cdef int i, j
+#     for j in range(m):
+#         value = unique_arr[j]
+#         map_pair = (value, j)
+#         enumeration_map.insert(map_pair)
+#     for i in range(n):
+#         value = arr[i]
+#         return_arr[i] = enumeration_map[value]
+#     return return_arr
 
 def pivot(df, index, column, value):
     """
@@ -84,19 +84,19 @@ def pivot(df, index, column, value):
     value: string, name of column that contains as values the values of the pivot table. values must be of type np.float64.
     Returns a pandas dataframe
     """
-    tick = time.perf_counter()
-    idx_arr, idx_arr_unique = df[index].factorize()
-    col_arr, col_arr_unique = df[column].factorize()
-    print(1, time.perf_counter() - tick)
+    #tick = time.perf_counter()
+    idx_arr, idx_arr_unique = df[index].factorize(sort=True)
+    col_arr, col_arr_unique = df[column].factorize(sort=True)
+    #print(1, time.perf_counter() - tick)
     n_idx = idx_arr_unique.shape[0]
     n_col = col_arr_unique.shape[0]
-    tick = time.perf_counter()
+    #tick = time.perf_counter()
     pivot_arr = pivot_cython(idx_arr, col_arr, df[value].to_numpy(), n_idx, n_col)
-    print(2, time.perf_counter() - tick)
-    tick = time.perf_counter()
+    #print(2, time.perf_counter() - tick)
+    #tick = time.perf_counter()
     arr = np.array(pivot_arr)
     pivot_df = pd.DataFrame(arr, index=idx_arr_unique, columns=col_arr_unique)
-    print(3, time.perf_counter() - tick)
+    #print(3, time.perf_counter() - tick)
     return pivot_df
 
 cdef double[:, :] pivot_cython(long[:] idx_arr, long[:] col_arr, double[:] value_arr, int N, int M):
--- test_pivot.py
+++ test_pivot.py
@@ -3,26 +3,40 @@ import pandas as pd
 import numpy as np
 import time
 
+# create data
+    
 column1 = 'to_be_idx'
 column2 = 'to_be_col'
 column3 = 'value'
 
-row1 = ['111', 'sub2', 7]
-row2 = ['222', 'sub2', 2]
-row3 = ['222', 'sub1', 11]
-data = [row1, row2, row3]
+n_rows = 100
+n_cols = 10
+n_idx = 10
+col1 = ['idx{}'.format(x) for x in np.random.randint(0, n_idx, size=n_rows)]
+col2 = ['col{}'.format(x) for x in np.random.randint(0, n_cols, size=n_rows)]
+col3 = [x for x in np.random.normal(size=n_rows)]
+
+data = np.transpose([col1, col2, col3])
 df = pd.DataFrame(data, columns=[column1, column2, column3], index=range(len(data)))
 df[column3] = df[column3].astype(np.float64)
-print(df)
+#print(df)
+
+# time
 
 msg = 'cython'
 tick = time.perf_counter()
 pivot_cython = pivot.pivot(df, index=column1, column=column2, value=column3)
 print(msg, time.perf_counter() - tick)
-print(pivot_cython)
+#print(pivot_cython)
 
 msg = 'pandas'
 tick = time.perf_counter()
-pivot_pandas = df.pivot_table(index=column1, columns=[column2], values=column3, fill_value=0)
+pivot_pandas = df.pivot_table(index=column1, columns=[column2], values=column3, fill_value=0.0, aggfunc='sum')
 print(msg, time.perf_counter() - tick)
-print(pivot_pandas)
+#print(pivot_pandas)
+
+# check results are equal
+
+print('componentwise equal: ', (pivot_cython.to_numpy() == pivot_pandas.to_numpy()).all())
+epsilon = 1e-8
+print('componentwise within {} :'.format(epsilon), (np.absolute(pivot_cython.to_numpy() - pivot_pandas.to_numpy()) < epsilon).all())
