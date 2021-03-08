3ccfa6693b805a327ffe843837edd64fc4931d6e
SethEBaldwin <seth.e.baldwin@gmail.com>
2021-03-08 13:07:03 -0500

    somewhat reasonable multicolumns

--- pivot.pyx
+++ pivot.pyx
@@ -86,30 +86,28 @@ def pivot(df, index, columns, values, agg='mean'):
     df: pandas dataframe
     index: string, name of column that you want to become the index. 
     columns: string or list, name(s) of column that contains as values the columns of the pivot table. 
-        currently only fast if columns is a string or a list of two strings that correspond to columns with dtype string.
     values: string, name of column that contains as values the values of the pivot table. values must be of type np.float64.
     agg: string, name of aggregation function. must be 'sum' or 'mean'.
     Returns a pandas dataframe
     """
     assert agg in ['sum', 'mean']
-    tick = time.perf_counter()
+    #tick = time.perf_counter()
     idx_arr, idx_arr_unique = df[index].factorize(sort=True)
     if isinstance(columns, str):
         col_arr, col_arr_unique = df[columns].factorize(sort=True)
-    elif len(columns) == 2 and isinstance(df.loc[0, columns[0]], str) and isinstance(df.loc[0, columns[1]], str):
-        tick = time.perf_counter()
-        columns_pairs_arr = pd.Series(to_pairs(df[columns].to_numpy()))
-        print('to pairs', time.perf_counter() - tick)
-        col_arr, col_arr_unique = columns_pairs_arr.factorize(sort=True)
+    #elif len(columns) == 2 and isinstance(df.loc[0, columns[0]], str) and isinstance(df.loc[0, columns[1]], str):
+    #    tick = time.perf_counter()
+    #    columns_pairs_series = pd.Series(to_pairs(df[columns].to_numpy()))
+    #    print('to pairs', time.perf_counter() - tick)
+    #    col_arr, col_arr_unique = columns_pairs_series.factorize(sort=True)
     else:
-        """This is going to be slow, so just resort to pandas method. TODO: improve this"""
-        print('cannot provide speedup, resorting to calling pd.pivot_table')
-        # tick = time.perf_counter()
-        # columns_series = df[columns].apply(lambda x: tuple(x), axis=1)
-        # print('apply', time.perf_counter() - tick)
-        # col_arr, col_arr_unique = columns_series.factorize(sort=True)
-        return df.pivot_table(index=index, columns=columns, values=values, fill_value=0.0, aggfunc=agg)
-    print(1, time.perf_counter() - tick)
+        tick1 = time.perf_counter()
+        #columns_series = df[columns].apply(lambda x: tuple(x), axis=1)
+        columns_series = pd.Series([tuple(x) for x in df[columns].to_numpy()])
+        print('apply', time.perf_counter() - tick1)
+        col_arr, col_arr_unique = columns_series.factorize(sort=True)
+        #return df.pivot_table(index=index, columns=columns, values=values, fill_value=0.0, aggfunc=agg)
+    #print(1, time.perf_counter() - tick)
     n_idx = idx_arr_unique.shape[0]
     n_col = col_arr_unique.shape[0]
     #tick = time.perf_counter()
@@ -130,14 +128,14 @@ def pivot(df, index, columns, values, agg='mean'):
     #print(3, time.perf_counter() - tick)
     return pivot_df
 
-cdef vector[cpair[string, string]] to_pairs(np.ndarray arr):
-    cdef cpair[string, string] tup
-    cdef int i
-    cdef vector[cpair[string, string]] pairs
-    for i in range(arr.shape[0]):
-        tup = (arr[i, 0], arr[i, 1])
-        pairs.push_back(tup)
-    return pairs
+# cdef vector[cpair[string, string]] to_pairs(np.ndarray arr):
+#     cdef cpair[string, string] tup
+#     cdef int i
+#     cdef vector[cpair[string, string]] pairs
+#     for i in range(arr.shape[0]):
+#         tup = (arr[i, 0], arr[i, 1])
+#         pairs.push_back(tup)
+#     return pairs
 
 cdef double[:, :] pivot_cython_sum(long[:] idx_arr, long[:] col_arr, double[:] value_arr, int N, int M):
     cdef double[:, :] pivot_arr = np.zeros((N, M), dtype=np.float64)
